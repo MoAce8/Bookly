276394701b9399a0bf44b281985509313d843268
MoAce8 <mohammedtharwat21@gmail.com>
2024-09-17 23:20:10 +0300

    create home appbar

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,10 +1,29 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        leadingWidth: 0,
+        backgroundColor: kPrimaryColor,
+        title: Image.asset(
+          AssetsData.logo,
+          height: 24,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass),
+          ),
+        ],
+      ),
+    );
   }
 }
